30fcd950cb5d6788c27d000e72453ef376b9cd57
Adil Khursheed <mughaladil571@gmail.com>
2024-01-30 02:30:09 +0500

    Implemented bubble_sort

---
+++ SortingAlgorithms/bubble_sort.py
@@ -0,0 +1,14 @@
+def bubble_sort(array):
+    n = len(array)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if array[j] > array[j + 1]:
+                temp = array[j + 1]
+                array[j + 1] = array[j]
+                array[j] = temp
+    return array
+
+
+numbers = [50, 5, 70, 40, 50]
+numbers = bubble_sort(numbers)
+print(numbers)
